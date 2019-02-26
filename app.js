962ca6ac9009f7a9d2d1bc5ef8300cbaf21ccea5
Karam Singh <karamsingh175@gmail.com>
2019-02-26 00:44:12 -0500

    deployment t4

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ if(process.env.NODE_ENV === 'production') {
 }
 
 //build mode
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/client/public/index.html'));
 });
 
